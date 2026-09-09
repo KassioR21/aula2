c0eb2a8671abf2b75bcea57d365acd1499a9d474
KassioR21 <kassioronald04@gmail.com>
2026-09-09 11:46:54 -0300

    Add files via upload

---
+++ adivinha.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class adivinha {
+    
+    public static void main (String [] args){
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite o seu palpite");
+        int palpite = entrada.nextInt();
+
+        if (palpite == 15){
+            System.err.println("Parabéns, você acertou! O número secreto é 15.");
+        }else if (palpite < 15) {
+            System.out.println("Seu palpite está baixo.");
+
+        }else{
+            System.out.println("Seu palpite está acima.");
+            
+        }
+        entrada.close();    
+    }
+}
